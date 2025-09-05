a7b74c1b1f0ba4c435e8781550e97a7502b0d13d
Affan840 <muhammadaffan070000@gmail.com>
2025-09-05 14:31:52 +0500

    create message functionality

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,61 @@
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  const currentUserId = req.user._id;
+  try {
+    const filteredUsers = await User.find({
+      _id: { $ne: currentUserId },
+    }).select("-password");
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error in getUsersForSidebar:", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messsages = await Message.find({
+      $or: [
+        { myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    }).sort({ createdAt: 1 });
+
+    res.status(200).json(messsages);
+  } catch (error) {
+    console.error("Error in getMessages:", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(profilePic);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error("Error in sendMessage:", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};

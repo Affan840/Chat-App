fb29862fbe8e9b82ca82840ac4ca8128c7babebc
Affan840 <muhammadaffan070000@gmail.com>
2025-09-14 11:09:08 +0500

    fix socket error

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,37 +1,40 @@
-import {Server} from 'socket.io';
-import http from 'http';
-import express from 'express';
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
-    cors: {
-        origin: process.env.FRONTEND_URL || "http://localhost:5173"
-    }
-})
-
-export function getSocketReceiverId(userId) {
-    return usersSocketMap[userId];
-}
-
-const usersSocketMap = {};
-
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
-
-    const userId = socket.handshake.query.userId;
-    if (userId) {
-        usersSocketMap[userId] = socket.id;
-        console.log('User connected:', userId);
-    }
-
-    io.emit("getOnlineUsers", Object.keys(usersSocketMap));
-
-   socket.on("disconnect", () => {
-       console.log("user disconnected");
-       delete usersSocketMap[userId];
-       io.emit("getOnlineUsers", Object.keys(usersSocketMap));
-   });
+  cors: {
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  },
 });
 
-export {io, app, server};
+// store userId -> socketId
+const usersSocketMap = {};
+
+export function getSocketReceiverId(userId) {
+  return usersSocketMap[userId];
+}
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+
+  const userId = socket.handshake.query.userId;
+  if (userId) {
+    usersSocketMap[userId] = socket.id;
+    console.log("User connected:", userId);
+  }
+
+  io.emit("getOnlineUsers", Object.keys(usersSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+    delete usersSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(usersSocketMap));
+  });
+});
+
+export { io, app, server };

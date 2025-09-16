d9adcf05a43f08cfdc6044bfdcbea1a75f071943
Affan840 <muhammadaffan070000@gmail.com>
2025-09-16 10:55:19 +0500

    fix cookie issue

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // set to true in production
-        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     });
 
     return token;
